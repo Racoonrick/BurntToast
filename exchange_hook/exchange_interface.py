f1d875f55491772ad13098e7d04240c83a06c3e0
brenton <greatbrenton@gmail.com>
2018-06-12 05:27:04 +0000

    exchange_interface.py - Added ability to read key, secret & passphrase from JSON File
    Make a login.json file with GDAX key, b64secret, and passphrase
    and put it in the same directory that you run commands from.
    
    Trying out loginPath and exchangeName as static members of exchange class
    
    dongbot file has working examples of how to call the static and non-static functions

--- exchange_hook/exchange_interface.py
+++ exchange_hook/exchange_interface.py
@@ -1,14 +1,32 @@
-import data_handle
+from data_handling.data_handle import data_handle as dh
 import json
+#import gdax
+
+print ("test1")
+
+def toptest():
+	print ("test2")
 
 class exchange:
-	#def __init__(self,exchangeName):
+	exchangeName="GDAX"
+	loginPath="login.json" #Copy the JSON file to the directory you execute your program from
+	# def __init__(self):
 		#self.exchangeName = exchangeName
 
-	def accountInfo(infoPath,exchangeName="GDAX"):
-		if exchangeName == "GDAX":
-			return data_handle.data_handle(infoPath).dictRead()
+	def accountInfo(self):
+		if exchange.exchangeName == "GDAX":
+			data = dh(exchange.loginPath).dictRead()
+			print (data['key'])
+			print (data['b64secret'])
+			print (data['passphrase'])
 		else:
 			return {}
-
 	
+	@staticmethod
+	def test():
+		print ("test4")
+
+	# def getBalance(self):
+	# 	if exchangeName =="GDAX":
+			
+	# 	return
