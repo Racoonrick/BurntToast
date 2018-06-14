8fe9f1182b250c0f57a0759cbb8ae83feea64d28
Racoonrick <dikkywahl@gmail.com>
2018-06-13 21:31:11 -0700

    Added a shell for main.py in the main directory

---
+++ main.py
@@ -0,0 +1,13 @@
+from data_handling.data_handle import data_handle
+from exchange_hook.exchange_interface import exchange
+
+
+def main():
+ 	print("Hello world!")
+ 	
+
+
+
+
+# if __name__ == "__main__":main()
+print("hi")
