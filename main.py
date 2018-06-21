24b5e88868042a2e39941dca9a698741ef64a012
brenton <greatbrenton@gmail.com>
2018-06-21 01:25:44 +0000

    main.py - Added Skeleton for Bot's Main Functionality
    main.py contains an infinite loop which repeats 3 steps indefinitely:
    1. Read Configuration Parameters from bot_params.json
    2. Execute trading_heart code
    3. Wait 1 Second

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 from data_handling.data_handle import data_handle
 from exchange_hook.exchange_interface import exchange
+import time
+import os.path
 
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+class trading_heart:
+    def __init__(self, params):
+        self.params=params
+        self.ex=exchange("GDAX")
+        print("init")
+
+    def trade(self, params):
+        print("Trade")
+        print("Trade Param 2: ", params['param2'])  #Proof that Parameters are read from bot_params.json
+        #self.ex.printBalances()                     #Proof that Trading Heart is interacting with GDAX
 
 def main():
- 	print("Hello world!")
+    confPath = os.path.join(current_path, "./bot_params.json")
+    tradeConf = data_handle(confPath).dictRead()
+    heart=trading_heart(tradeConf)
  	
+	#This is the Main InfiniteLoop
+    while True:
+        #Read Configuration Options
+        tradeConf = data_handle(confPath).dictRead()
+        # print("Trade Param 1: ", tradeConf['param1']) #Param Read Test
+        
+        #Execute Trading Heart
+        heart.trade(tradeConf)
+        
+        #Pause
+        time.sleep(1)
 
-
-
-
-# if __name__ == "__main__":main()
-print("hi")
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("")
+        print("Bye!")
