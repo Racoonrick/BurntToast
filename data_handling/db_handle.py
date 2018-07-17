f7c75e7a7b982f5c08de6c77eff421dfe598f7c1
brenton <greatbrenton@gmail.com>
2018-07-17 05:38:53 +0000

    data_handling/db_handle.py - add most basic database functionality to db_handle.py
    db_handle can now create a database, write trades, and read trades.  test.py has
    an example creation, insertions, read, and print
    
    - create_db() - create new database.  This will return error if database exists
    - insert_trade() - Insert new trade into trades table of database.
    - read_trade() - reads the first trade in the database
    - read_trade_from_seq() - reads returns a trade of a given sequence
    - print_trades() - prints all trades, ordered by sequence

--- data_handling/db_handle.py
+++ data_handling/db_handle.py
@@ -2,11 +2,49 @@ import sqlite3
 
 class db_handle(object):
 	"""docstring for db_handle"""
-	def __init__(self):
-		#Hi
+	# def __init__(self):
+	# 	#Hi
 
 	def create_db(self,db_name):
 		conn = sqlite3.connect(db_name)
 		c = conn.cursor()
 		c.execute('''CREATE TABLE trades
-             (sequence real, price real, size real, stime real)''')
+			(sequence integer, price real, size real, stime real)''')
+			
+	def insert_trade(self, db_name, sequence, price, size, stime):
+		conn = sqlite3.connect(db_name)
+		c = conn.cursor()
+		
+		trade = [(sequence, price, size, stime)]
+		
+		c.executemany('INSERT into trades VALUES (?,?,?,?)', trade)
+		
+		conn.commit()
+		
+	def read_trade(self, db_name, sequence): #brenton this just reads the first line in the database
+		conn = sqlite3.connect(db_name)
+		c = conn.cursor()
+		
+		c.execute('SELECT * FROM trades')
+		
+		return c.fetchone()
+		
+	def read_trade_from_seq(self, db_name, sequence):
+		conn = sqlite3.connect(db_name)
+		c = conn.cursor()
+		
+		sql_seq = (sequence,)
+		
+		c.execute('SELECT * FROM trades WHERE sequence=?', sql_seq)
+		
+		return c.fetchone()
+		
+	def print_trades(self, db_name):
+		conn = sqlite3.connect(db_name)
+		c = conn.cursor()
+		for row in c.execute('SELECT * FROM trades ORDER BY sequence'):
+			print (row)
+
+	# def open_db(self, db_name)
+	# 	conn = sqlite3.connect(db_name)
+	# 	c = conn.cursor()
