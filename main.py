0a60835f3e0273aa0a8d0fb2651147bbb82e72df
Racoonrick <dikkywahl@gmail.com>
2018-07-24 12:39:44 -0700

    Added trade recording capabilities in main.p

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class trading_heart:
         self.buytarget = 0.0
         self.seltarget = 0.0
         print("init")
+        self.data_trades = data_handle("trades_record")
+        self.data_trades.fopen("a")
 
     def trade(self, params):
 
@@ -20,6 +22,9 @@ class trading_heart:
         print("Sel Target: ", self.seltarget, "\n")
 
         if self.ex.getOrders() == [[]]:  # Only place order if no open orders
+            #Opens data_trades for write
+            self.data_trades.fopen("a")
+
             ticker_price = float(self.ex.auth_client.get_product_ticker('BTC-USD')['price'])
             
             self.buytarget = ticker_price + float(params['buy buffer'])
@@ -29,8 +34,19 @@ class trading_heart:
             self.seltarget_str ='%.2f'% self.seltarget
 
             self.ex.buy(self.buytarget_str, str(params['quantity']), 'BTC-USD')
+            self.RecordTrades("buy",self.buytarget_str,str(params['quantity']))
             self.ex.sell(self.seltarget_str, str(params['quantity']), 'BTC-USD')
-            
+            self.RecordTrades("sell",self.seltarget_str,str(params['quantity']))
+
+            #Closes file for recording trading data
+            self.data_trades.fclose()
+
+    def RecordTrades(self,trade_type,target,quantity):
+        self.trade_dict_hold = {}
+        self.trade_dict_hold['trade_type'] = trade_type
+        self.trade_dict_hold['target']= target
+        self.trade_dict_hold['quantity'] = quantity
+        self.data_trades.fwrite(json.dumps(self.trade_dict_hold))        
 
 def main():
     confPath = os.path.join(current_path, "./bot_params.json")
