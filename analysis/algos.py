2548b4eff9b529be02fd1cdfc405df005365a086
Racoonrick <dikkywahl@gmail.com>
2018-08-05 18:12:08 -0700

    Updated algos to include the linear regression function. Added a second slider to shift the x-axis more quickly instead of just scrolling.

--- analysis/algos.py
+++ analysis/algos.py
@@ -1,41 +1,5 @@
-# from data_handling.db_handle import db_handle as dbh
-# import sqlite3
-# import sys
-# import matplotlib
-# import matplotlib.pyplot as plt
-# from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-# from matplotlib.figure import Figure 
-# from matplotlib import style
-
-
-# db_path ="C:/Users/Ricky/Documents/Work/BurntToast/exchange_hook/websocket_trades_db"
-# tname = 'trades'
-# tcolumns = '(sequence integer, price real, size real, time real)'
-# algo_dbh = dbh(db_path,tname,tcolumns)
-# #algo_dbh.conn.execute('SELECT * FROM trades ORDER BY sequence ASC')
-# algo_dbh.c.execute('SELECT price, size, time FROM trades')
-# #algo_dbh.print_trades()
-# data = algo_dbh.c.fetchall()
-
-# #print(data)
-# pricev = []
-# sizev = []
-# timev = []
-# for row in data:
-# 	pricev.append(row[0])
-# 	sizev.append(row[1])
-# 	timev.append(row[2])
-
-# fig = plt.figure()
-# ax1 = fig.add_subplot(111)
-# ax1.set_title("Price vs Time")
-# ax1.set_ylabel("Price [$]")
-# ax1.set_xlabel("Time [s]")
-# plt.figure()
-# plt.plot(timev,pricev,'-',label="Price",color='red')
-# ax1.plot(timev,pricev,'-',label="Price",color='red')
-
 from data_handling.db_handle import db_handle as dbh
+from analysis.Derivative import Derivative
 import sqlite3
 import sys
 import time
@@ -56,9 +20,9 @@ class App(QMainWindow):
 		super().__init__()
 		self.left = 100
 		self.top = 100
-		self.title = 'PyQt5 matplotlib example - pythonspot.com'
+		self.title = 'Burnt Toast - BTC Visual'
 		self.width = 900
-		self.height = 600
+		self.height = 620
 		
 		self.initUI()
 
@@ -82,16 +46,25 @@ class App(QMainWindow):
 		button2.resize(70,50)
 		button2.clicked.connect(self.reset_plot)
 
+		#This is the zoom slider initialization
 		self.sl = QSlider(QtCore.Qt.Horizontal,self)
 		self.sl.move(0,550)
-		self.sl.sliderReleased.connect(self.sl_release)
+		self.sl.sliderReleased.connect(self.sl_zoom_release)
+
+		#This is the shift slider initialization
+		self.s2 = QSlider(QtCore.Qt.Horizontal,self)
+		self.s2.move(0,580)
+		self.s2.resize(200,50)
+		self.s2.sliderReleased.connect(self.sl_shift_release)
 
 		self.x = 0
 
 		self.show()
-	def sl_release(self):
-		print(self.sl.value())
+	def sl_zoom_release(self):
 		self.m.SlZoom(self.sl.value())
+
+	def sl_shift_release(self):
+		self.m.SlideShift(self.s2.value())
 
 	def on_click(self):
 		self.m.plot()
@@ -109,16 +82,23 @@ class PlotCanvas(FigureCanvas):
 	def __init__(self, parent=None, width=5, height=4, dpi=150):
 		self.d1 = 0
 		self.d2 = 0
+		#This sets the maximum time you will see
+		#from the recorded database
+		self.full_window = 1*24*60*60
 		self.pricev,self.sizev,self.timev = self.ParseData()
 		self.mpricev,self.msizev,self.mtimev = self.ParseMyTrade()
 		self.xa = min(self.timev)
 		self.xb = max(self.timev)
 		self.xcenter = (self.xb-self.xa)/2+self.xa
-		print(self.xcenter)
 		self.min_x = self.xa
 		self.max_x = self.xb
 		self.x_wid = self.max_x - self.min_x
-		print(self.x_wid)
+		
+		########################################
+		#Initialization for Linear Regression
+		self.fitWindow = 30*60
+		########################################
+
 		fig = Figure(figsize=(width, height), dpi=dpi)
 		self.axes = fig.add_subplot(111)
 
@@ -136,24 +116,29 @@ class PlotCanvas(FigureCanvas):
 		ax.figure.clear()
 		ax = self.figure.add_subplot(111)
 		ax.axes.set_xlim(self.xa,self.xb)
-		ax.plot(self.timev[:],self.pricev[:], 'r-',linewidth=0.2)
-		ax.plot(self.mtimev[:],self.mpricev[:], 'go',linewidth=5)
+		ax.axes.set_ylim(min(self.pricev)-100,max(self.pricev)+100)
+		ax.plot(self.timev,self.pricev, 'r-',linewidth=0.2)
+		ax.plot(self.mtimev,self.mpricev, 'go',linewidth=5)
+		ax.plot(self.xfit,self.yfit,'b-',linewidth=.2)
 
 
-		print(self.xa,self.xb)
-		ax.set_title('PyQt Matplotlib Example')
+		ax.set_title('BTC Trade Information')
 		self.draw()
 
 	def plot(self):
 		self.pricev,self.sizev,self.timev = self.ParseData()
 		self.mpricev,self.msizev,self.mtimev = self.ParseMyTrade()
+		self.xfit,self.yfit = self.LinearFit(self.fitWindow)
 		self.alen = len(self.timev)
+		self.min_x = min(self.timev)
+		self.max_x = max(self.timev)
+		self.x_wid = self.max_x - self.min_x
 		self.update_plot()
-	
-	def AxesScale(self):
-		max_x = math.max(self.timev)
-		min_x = math.min(self.timev)
-		
+
+	def LinearFit(self,fitWindow):
+		slope,intercept,xfit = Derivative().FitDataLinear(fitWindow,self.timev,self.pricev)
+		yfit = [i*slope+intercept for i in xfit]
+		return xfit, yfit
 
 	def SlZoom(self,sl_val=10):
 
@@ -161,30 +146,46 @@ class PlotCanvas(FigureCanvas):
 			scale = (99/100)
 		else:
 			scale = ((100-sl_val)/100)
-		if self.xcenter - self.x_wid/2*scale > min(self.timev) :
+		if self.xcenter - self.x_wid/2*scale > self.min_x :
 			self.xa = self.xcenter - self.x_wid/2*scale
 		else:
 			self.xa = self.min_x
-		if self.xcenter + self.x_wid/2*scale < max(self.timev):
+		if self.xcenter + self.x_wid/2*scale < self.max_x:
 			self.xb = self.xcenter + self.x_wid/2*scale
 		else:
 			self.xb = self.max_x
-		print(self.xcenter)
+		self.update_plot()
+
+	def SlideShift(self,sl_slide):
+		#Slider shift control
+		window_x = self.max_x - self.min_x
+		scale = sl_slide/100
+		newcenter = window_x * scale + self.min_x
+		oldcenter = (self.xb-self.xa)/2+self.xa
+		sl_shift = newcenter-oldcenter
+		if self.xa + sl_shift > self.min_x:
+			self.xa = self.xa + sl_shift
+		else:
+			self.xa = self.min_x
+
+		if self.xb + sl_shift < self.max_x:
+			self.xb = self.xb + sl_shift
+		else:
+			self.xb = self.max_x-1
+		self.xcenter = newcenter
 		self.update_plot()
 
 	def WheelShift(self,sl_slide):
-		print(self.xa,self.xb)
-		if self.xa + sl_slide > min(self.timev):
+		if self.xa + sl_slide > self.min_x:
 			self.xa = self.xa + sl_slide
 		else:
-			self.xa = min(self.timev)
+			self.xa = self.min_x
 
 		if self.xb + sl_slide < self.max_x:
 			self.xb = self.xb + sl_slide
 		else:
 			self.xb = self.max_x-1
 		self.xcenter = (self.xb-self.xa)/2+self.xa
-		print("centerrrr",self.xcenter)
 
 		self.update_plot()
 	def DefaultPlot(self):
@@ -226,13 +227,14 @@ class PlotCanvas(FigureCanvas):
 		pricev = []
 		sizev = []
 		timev = []
+
 		for row in data:
 			pricev.append(row[0])
 			sizev.append(row[1])
 			timev.append(row[2])
 
-		self.max_x = max(timev)
-		return pricev,sizev,timev;
+		chopindex = Derivative().FrameTime(self.full_window,timev)
+		return pricev[-chopindex:],sizev[-chopindex:],timev[-chopindex:];
 
 
 if __name__ == '__main__':
