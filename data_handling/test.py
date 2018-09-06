34652c5377ba702682a1fb7f928f582a81781182
Racoonrick <dikkywahl@gmail.com>
2018-09-05 22:29:29 -0700

    Added test.py

--- data_handling/test.py
+++ data_handling/test.py
@@ -1,6 +1,6 @@
 import gdax
 import data_handle
-
+from data_handling.db_handle import db_handle as dbh
 import db_handle
 
 from exchange_hook.exchange_interface import exchange
@@ -36,35 +36,35 @@ public_client = gdax.PublicClient()
 
 
 #################################################################db_handle Stuff
-test_str = '(sequence integer, price real, size real, time real)'
-db = db_handle.db_handle("fake_trades",'trades',test_str)
+# test_str = '(sequence integer, price real, size real, time real)'
+# db = db_handle.db_handle("fake_trades",'trades',test_str)
 
-#Here are a bunch of Inserts to fill up the database
-#self.trade_dict_hold
-data = {'time':1532723000,'price':8240,'size':1,'sequence':100}
-db.insert_trade(data)
-data = {'time':1532728000,'price':8200,'size':1,'sequence':101}
-db.insert_trade(data)
-data = {'time':1532730350,'price':8180,'size':1,'sequence':102}
-db.insert_trade(data)
-data = {'time':1532733400,'price':8140,'size':1,'sequence':103}
-db.insert_trade(data)
-data = {'time':1532733450,'price':8150,'size':1,'sequence':104}
-db.insert_trade(data)
+# #Here are a bunch of Inserts to fill up the database
+# #self.trade_dict_hold
+# data = {'time':1532723000,'price':8240,'size':1,'sequence':100}
+# db.insert_trade(data)
+# data = {'time':1532728000,'price':8200,'size':1,'sequence':101}
+# db.insert_trade(data)
+# data = {'time':1532730350,'price':8180,'size':1,'sequence':102}
+# db.insert_trade(data)
+# data = {'time':1532733400,'price':8140,'size':1,'sequence':103}
+# db.insert_trade(data)
+# data = {'time':1532733450,'price':8150,'size':1,'sequence':104}
+# db.insert_trade(data)
 
-print (db.read_trade_from_seq(6))
+# print (db.read_trade_from_seq(6))
 
-db.print_trades()
+# db.print_trades()
 
-print([description[0] for description in db.c.description])
-print("End of Test")
-array1 = []
-i = 0
-d = {'l':0,'a':1,'b':2,'c':3}
-for keys in d.keys():
-	array1.insert(i,d[keys])
-	i=i+1
-print(array1)
+# print([description[0] for description in db.c.description])
+# print("End of Test")
+# array1 = []
+# i = 0
+# d = {'l':0,'a':1,'b':2,'c':3}
+# for keys in d.keys():
+# 	array1.insert(i,d[keys])
+# 	i=i+1
+# print(array1)
 ################################################################Exchange Stuff
 
 # ex=exchange("GDAX")
@@ -87,3 +87,11 @@ print(array1)
 # db = db_handle.db_handle("websocket_trades_db",'trades',test_str)
 # db.conn.execute("SELECT * FROM trades ORDER BY sequence ASC")
 # db.print_trades()
+
+db_path ="C:/Users/Ricky/Documents/Work/BurntToast/fake_trades.db"
+tname = 'fake_trades'
+tcolumns = '(sequence integer, price real, size real, time real, slope real, cash real, btc real, tradetype char(4))'
+algo_dbh = dbh(db_path,tname,tcolumns)
+algo_dbh.c.execute('SELECT * FROM fake_trades')
+data = algo_dbh.c.fetchall()
+print(data)
